b2f8bfa23cd8cd1ddd3455d30e79226cfeb1bc5e
juanf1990 <juanandres15@gmail.com>
2022-12-20 23:16:37 +0000

    added multer add modified the modify controller

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -4,19 +4,15 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 
-/* TODO : Add multer middleware to handle file upload */
-
+const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
 
-
-/* TODO: Add correct paths for the following routes */
 router.get('/', auth, sauceCtrl.getAllSauces);
-/* TODO : Line 17: two middlewares for creating a sauce */
-router.post('/', sauceCtrl.createSauce);
-router.get('/sauces/:id', sauceCtrl.getOneSauce);
-router.put('/api/sauces/:id', sauceCtrl.modifySauce);
-router.delete('/api/sauces/:id', sauceCtrl.deleteSauce);
-router.post('/api/sauces/:id/like', sauceCtrl.likeSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post(':id/like', sauceCtrl.likeSauce);
 
 module.exports = router;
