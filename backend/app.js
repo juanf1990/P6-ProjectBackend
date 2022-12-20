490744c75ceda90746d08695fe7fc2cbab08b386
juanf1990 <juanandres15@gmail.com>
2022-12-20 23:18:03 +0000

    updated

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
+const path = require('path');
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,18 +1,21 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
+    const url = req.protocol + '://' + req.get('host');
+    req.body.sauce = JSON.parse(req.body.sauce);
     const sauce = new Sauce({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
+        userId: req.body.sauce.userId,
+        name: req.body.sauce.name,
+        manufacturer: req.body.sauce.manufacturer,
+        description: req.body.sauce.description,
+        mainPepper: req.body.sauce.mainPepper,
+        imageUrl: url + '/images/' + req.file.filename,
+        heat: req.body.sauce.heat,
+        likes: req.body.sauce.likes,
+        dislikes: req.body.sauce.dislikes,
+        usersLiked: req.body.sauce.usersLiked,
+        usersDisliked: req.body.sauce.usersDisliked,
         });
     sauce.save().then(
         () => {
@@ -46,20 +49,40 @@ exports.getOneSauce = (req, res, next) => {
 }
 
 exports.modifySauce = (req, res, next) => {
-    const sauce = new Sauce({
-        _id: req.params.id,
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
-    });
+    let sauce = new Sauce({ _id: req.params._id });
+    if (req.file) {
+        const url = req.protocol + '://' + req.get('host');
+        req.body.sauce = JSON.parse(req.body.sauce);
+        sauce = {
+            _id: req.params.id,
+            userId: req.body.sauce.userId,
+            name: req.body.sauce.name,
+            manufacturer: req.body.sauce.manufacturer,
+            description: req.body.sauce.description,
+            mainPepper: req.body.sauce.mainPepper,
+            imageUrl: url + '/images/' + req.file.filename,
+            heat: req.body.sauce.heat,
+            likes: req.body.sauce.likes,
+            dislikes: req.body.sauce.dislikes,
+            usersLiked: req.body.sauce.usersLiked,
+            usersDisliked: req.body.sauce.usersDisliked,
+            };
+        } else {
+            sauce = {
+                _id: req.params.id,
+                userId: req.body.userId,
+                name: req.body.name,
+                manufacturer: req.body.manufacturer,
+                description: req.body.description,
+                mainPepper: req.body.mainPepper,
+                imageUrl: req.body.imageUrl,
+                heat: req.body.heat,
+                likes: req.body.likes,
+                dislikes: req.body.dislikes,
+                usersLiked: req.body.usersLiked,
+                usersDisliked: req.body.usersDisliked,
+            };
+        }
     Sauce.updateOne({_id: req.params.id}, sauce).then(
         () => {
             res.status(201).json({
@@ -77,33 +100,26 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id }).then(
-      (sauce) => {
-        if (!sauce) {
-          return res.status(404).json({
-            error: new Error('Sauce not found!')
-          });
+        (sauce) => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink('images/' + filename, () => {
+                Sauce.deleteOne({ _id: req.params.id }).then(
+                    () => {
+                        res.status(200).json({
+                            message: 'Deleted!'
+                        });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({
+                            error: error
+                        });
+                    }
+                );
+            }); 
         }
-        if (sauce.userId !== req.auth.userId) {
-          return res.status(401).json({
-            error: new Error('Request not authorised!')
-          });
-        }
-        Sauce.deleteOne({_id: req.params.id}).then(
-          () => {
-            res.status(200).json({
-              message: 'Deleted!'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(400).json({
-              error: error
-            });
-          }
-        );
-      }
     );
-  };
+}
 
 exports.likeSauce = (req, res, next) => {
     const { id } = req.params;
