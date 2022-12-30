6645215adabe96357432a07378c0550a2a776089
Juan Ferreira <juanandres15@gmail.com>
2022-12-30 17:22:46 +0000

    removed auth to put routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 
-const multer = require('../middleware/multer-config');
+const multer = require("../middleware/multer-config");
 
-const sauceCtrl = require('../controllers/sauce');
+const sauceCtrl = require("../controllers/sauce");
 
-router.get('/', auth, sauceCtrl.getAllSauces);
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post(':id/like', sauceCtrl.likeSauce);
+router.get("/", auth, sauceCtrl.getAllSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+router.put("/:id", multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.post(":id/like", auth, sauceCtrl.likeSauce);
 
 module.exports = router;
